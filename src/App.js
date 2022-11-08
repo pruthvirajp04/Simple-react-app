9c269d0754dceb5190b543d77785bd7ab94935c4
Pruthviraj <pruthviraj.p@somaiya.edu>
2022-11-09 02:38:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customers from './Customers'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    console.log("Host URL"+process.env.PUBLIC_URL);
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Exp-10</h1>
+          <br/>
+        
+        </header>
+          <Switch>
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/customerlist"/>
+                )}/>
+                 <Route exact path='/customerlist' component={Customers} />
+          </Switch>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
